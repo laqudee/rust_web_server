b198aaa017afdd18352383fc49e19ab1c4449f31
ZhaoYLong <2268678583@qq.com>
2023-03-11 13:18:09 +0800

    feat 构建REST API GET 结束

--- webservice/src/handlers.rs
+++ webservice/src/handlers.rs
@@ -36,6 +36,49 @@ pub async fn new_course(
     HttpResponse::Ok().json("Course added")
 }
 
+pub async fn get_courses_for_teacher(
+    app_state: web::Data<AppState>,
+    params: web::Path<usize>,
+) -> HttpResponse {
+    let teacher_id: usize = params.into_inner();
+
+    let filtered_courses = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .filter(|course| course.teacher_id == teacher_id)
+        .collect::<Vec<Course>>();
+
+    if filtered_courses.len() > 0 {
+        HttpResponse::Ok().json(filtered_courses)
+    } else {
+        HttpResponse::Ok().json("No courses found for teacher".to_string())
+    }
+}
+
+pub async fn get_course_detail(
+    app_state: web::Data<AppState>,
+    params: web::Path<(usize, usize)>,
+) -> HttpResponse {
+    let (teacher_id, course_id) = params.into_inner();
+    let selected_course = app_state
+        .courses
+        .lock()
+        .unwrap()
+        .clone()
+        .into_iter()
+        .find(|x| x.teacher_id == teacher_id && x.id == Some(course_id))
+        .ok_or("Course not Found");
+
+    if let Ok(course) = selected_course {
+        HttpResponse::Ok().json(course)
+    } else {
+        HttpResponse::Ok().json("course not found".to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,6 +99,30 @@ mod tests {
             courses: Mutex::new(vec![]),
         });
         let resp = new_course(course, app_state).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_all_courses_success() {
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+        let teacher_id: web::Path<usize> = web::Path::from(1);
+        let resp = get_courses_for_teacher(app_state, teacher_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_course_detail_success() {
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+        let params: web::Path<(usize, usize)> = web::Path::from((1, 1));
+        let resp = get_course_detail(app_state, params).await;
         assert_eq!(resp.status(), StatusCode::OK);
     }
 }
--- webservice/src/routers.rs
+++ webservice/src/routers.rs
@@ -6,5 +6,10 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
 }
 
 pub fn course_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/courses").route("/", web::post().to(new_course)));
+    cfg.service(
+        web::scope("/courses")
+            .route("/", web::post().to(new_course))
+            .route("/{user_id}", web::get().to(get_courses_for_teacher))
+            .route("/{user_id}/{course_id}", web::get().to(get_course_detail))
+    );
 }
